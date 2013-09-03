fa27180dda7f2b251a533a0342dac98e9c4cfbe3
Emil Blume <emil.blume@gmx.de>
2013-09-03 13:20:06 -0700

    Changed

--- data/templates/bootstrap/toolbar.inc.php
+++ data/templates/bootstrap/toolbar.inc.php
@@ -5,7 +5,7 @@ if ($userservice->isLoggedOn() && is_object($currentUser)) {
 ?>
   
 
-<div class="navbar navbar-inverse">
+<div class="navbar">
 		<div class="navbar-inner">
        
     <!-- Be sure to leave the brand out there if you want it shown -->
@@ -14,9 +14,18 @@ if ($userservice->isLoggedOn() && is_object($currentUser)) {
 		<ul class="nav">
     		<li><a href="<?php echo createURL('bookmarks', $cUsername); ?>"><?php echo T_('Bookmarks'); ?></a></li>
 				<li><a href="<?php echo createURL('alltags', $cUsername); ?>"><?php echo T_('Tags'); ?></a></li>
-				<li><a href="<?php echo createURL('watchlist', $cUsername); ?>"><?php echo T_('Watchlist'); ?></a></li>
-				<li><a href="<?php echo $userservice->getProfileUrl($cUserId, $cUsername); ?>"><?php echo T_('Profile'); ?></a></li>
+
+		<li class="dropdown">
+    <a class="dropdown-toggle" id="dLabel" role="button" data-toggle="dropdown" data-target="#" href="/page.html">Dropdown<b class="caret"></b></a>
+    <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+		<li><a href="<?php echo $userservice->getProfileUrl($cUserId, $cUsername); ?>"><?php echo T_('Profile'); ?></a></li>
+		<li><a href="<?php echo createURL('watchlist', $cUsername); ?>"><?php echo T_('Watchlist'); ?></a></li>
+    </ul>
+    </li>
+
 		</ul>
+
+
 
 		<form class="navbar-search pull-right">
     <input type="text" class="search-query" placeholder="Search">
